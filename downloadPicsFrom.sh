8b43a49f9faf6a2f83e28b30a5d9c25eb9460200
Franek <panczo.mirecki@gmail.com>
2020-05-12 16:12:24 +0200

    working version saute
    downloads images
    TODO comments, documentation, detail

--- downloadPicsFrom.sh
+++ downloadPicsFrom.sh
@@ -8,8 +8,27 @@ curl $url -o .site -s
 regexGetImg="img.+src=\"[^\"]+\.(png|jpg|gif)"
 regexRemoveAttrName="[^\"]+\.(png|jpg|gif)"
 
-imgUrls=$(grep -o -E $regexGetImg ./.site | grep -o -E $regexRemoveAttrName)
+imgUrls=$(grep -oE $regexGetImg ./.site | grep -oE $regexRemoveAttrName)
 
-# echo ${imgUrls[@]}
+printf "${imgUrls[@]}\n" > ./.urls
 
+for img in $imgUrls
+do
+    filename="`echo $img | grep -oE "[^\/]+\.(png|jpg|gif)$"`"
+    if [[ "$img" =~ ^\/\/.+ ]]
+    then
+        curl "https:$img" -o ./$filename -s
+    elif [[ "$img" =~ ^https.+ ]]
+    then
+        curl $img -o ./$filename -s
+    elif [[ "$img" =~ ^\/[^\/].+ ]]
+    then
+        urlbase="`echo $url | grep -oE "^https:\/\/[^\/]+"`"
 
+        curl "$urlbase$img" -o ./$filename -s
+        echo "$urlbase$img"
+
+    fi
+done
+
+rm .site
