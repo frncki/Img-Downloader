c4948687e79cae05c97bb8429ce2a0852d8c2e89
Franek <panczo.mirecki@gmail.com>
2020-05-12 20:17:12 +0200

    final version (?)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Img-Downloader
+# Simple Image Downloader
+### that downloads every image file thats under img src element
+curl installed needed
+command for making script possible to execute: chmod 777 downloadPicsFrom.sh
+
--- downloadPicsFrom.sh
+++ downloadPicsFrom.sh
@@ -1,34 +1,39 @@
 #!/bin/bash
-# Franciszek Mirecki
-#chmod 755 downloadPicsFrom.sh
-#https://pl.wikipedia.org/wiki/Bizon
-url=$1
-curl $url -o .site -s
+#Franciszek Mirecki
 
-regexGetImg="img.+src=\"[^\"]+\.(png|jpg|gif)"
-regexRemoveAttrName="[^\"]+\.(png|jpg|gif)"
+url=$1 #set first argument as our URL
+curl $url -o ./.website -s  #download website source using curl
+                            #-o flag denotes the filename (in this case temporary file named ".website") of the downloaded URL
+                            #-s flag puts curl in silent mode
+if [ $? -ne 0 ] #if curl's exit status does not equal to 0 (success)
+	then
+		echo "ERROR! Invalid argument."
+		exit 1 # terminate the script
+elif [[ -f "./.website" ]] #if file .website exists
+then
+    regexGetImg="img.+src=\"[^\"]+\.(png|jpg|gif)" #gets img src elements from website source
+    regexRemoveAttrName="[^\"]+\.(png|jpg|gif)" #removes img src tags, and leaves clean file link
+    regexGetFileName="[^\/]+\.(png|jpg|gif)$" #gets name of downloaded file
+    regexGetURLBase="^https:\/\/[^\/]+" #gets base URL of provided website
 
-imgUrls=$(grep -oE $regexGetImg ./.site | grep -oE $regexRemoveAttrName)
+    imgUrls=$(grep -oE $regexGetImg ./.website | grep -oE $regexRemoveAttrName) #uses regexGetImg and regexRemoveAttrName for extracting clean files links
 
-printf "${imgUrls[@]}\n" > ./.urls
-
-for img in $imgUrls
-do
-    filename="`echo $img | grep -oE "[^\/]+\.(png|jpg|gif)$"`"
-    if [[ "$img" =~ ^\/\/.+ ]]
+    for img in $imgUrls #iterate through every link in imgUrls
+    do
+    fileName="`echo $img | grep -oE $regexGetFileName`" #uses regexGetFileName for getting name of file to download
+    if [[ "$img" =~ ^\/\/.+ ]] #if file link is absolute but starts with only "//" without "https:"
     then
-        curl "https:$img" -o ./$filename -s
-    elif [[ "$img" =~ ^https.+ ]]
+        curl "https:$img" -o ./$fileName -s #download file from given link with added "https:"
+    elif [[ "$img" =~ ^https.+ ]] #else if file link is absolute and full
     then
-        curl $img -o ./$filename -s
-    elif [[ "$img" =~ ^\/[^\/].+ ]]
+        curl $img -o ./$fileName -s #download file
+    elif [[ "$img" =~ ^\/[^\/].+ ]] #else if file link is relative
     then
-        urlbase="`echo $url | grep -oE "^https:\/\/[^\/]+"`"
-
-        curl "$urlbase$img" -o ./$filename -s
-        echo "$urlbase$img"
-
+        baseURL="`echo $url | grep -oE $regexGetURLBase`" #get website URL base
+        curl "$baseURL$img" -o ./$fileName -s #download file by merging website URL base and relative file link, which makes file link absolute
     fi
-done
+    done
 
-rm .site
+    rm ./.website #remove the temporary file
+    exit 0 # exit with status 0 (success)
+fi
